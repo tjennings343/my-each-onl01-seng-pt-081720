fbaefb3093f020b6b9b7881f24f6d237e289e8e1
Trey Jennings <tjennings343@gmail.com>
2020-08-27 05:44:40 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(learn)
+  x = 0
+  while x < learn.length
+    yield learn[x]
+    x = x + 1
+  end
+  learn
+  
+
+end
+
+
+my_each(["math", "science", "history"]) do |clas|
+  if clas.length > 4
+  end
 end
